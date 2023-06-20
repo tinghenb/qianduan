0081559dfd9a06c7652a8a61e60c906fc0b5d613
Tins <1537604848@qq.com>
2023-06-21 01:04:09 +0800

    s

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print("hello")
 
+=======
+print("hello" 
+>>>>>>> Stashed changes
+=======
+print("hello" 
+>>>>>>> Stashed changes
 =======
 print("hello" 
 >>>>>>> Stashed changes
